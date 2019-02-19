2190aa9f36080676337299510aac28c255c1b956
Michal Siron <michal.siron@merck.com>
2019-02-19 15:55:19 +0100

    Base repository and entity, project structure and basic controller

--- doc-site-app/src/main/java/cz/czu/thesis/ds/DsApplication.java
+++ doc-site-app/src/main/java/cz/czu/thesis/ds/DsApplication.java
@@ -1,9 +1,12 @@
 package cz.czu.thesis.ds;
 
-@SpringApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"cz.czu"})
 public class DsApplication{
 
     public static void main(String[] args) {
-
+        SpringApplication.run(DsApplication.class, args);
     }
 }
